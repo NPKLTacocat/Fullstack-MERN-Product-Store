38b967b316268c10ef34693661e18f8863404b36
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2025-05-07 11:56:13 +0700

    Set up endpoints to create, delete and get products from database

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,58 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error("Error in fetching products:", error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.post("/api/products", async (req, res) => {
+  const product = req.body;
+
+  if (!product.name || !product.price || !product.image) {
+    return res.status(400).json({
+      success: false,
+      message: "Missing fields, name, price, and image are required",
+    });
+  }
+
+  const newProduct = new Product(product);
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    console.error("Error in creating new product:", error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({
+      success: true,
+      message: "Product with the following ID was deleted",
+      id,
+    });
+  } catch (error) {
+    console.error("Error in deleting product:", error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
 });
 
 app.listen(5000, () => {
